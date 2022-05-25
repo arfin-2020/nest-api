a32dd9bf8d50901d21129c158735a87891904baf
unknown <arfinchowdhuryarif7@gmail.com>
2022-05-25 16:29:16 +0600

    api make done

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable({})
-export class AuthService {}
+export class AuthService {
+
+    signup(){
+        return {
+            message: "I am signed up"
+        };
+    }
+    signin(){
+        return {
+            message: "I am signed in"
+        };
+    }
+}
+
+
